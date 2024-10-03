3d37d83c154fe45c4f57a6c1fd7ea56785dd8f6b
promyr <jesusbelchior@outlook.com>
2024-10-02 21:40:38 -0300

    Exercicio feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "987654321", modelo: "Modelo 1", imei: "013285-7", memoria: 128);
+nokia.Ligar();
+nokia. InstalarAplicativo("Bet365");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("iPhone XR");
+Smartphone iphone = new Iphone(numero: "987452531", modelo: "Modelo 2", imei: "015499-8", memoria: 128);
+iphone. ReceberLigacao();
+iphone. InstalarAplicativo("Betano");
